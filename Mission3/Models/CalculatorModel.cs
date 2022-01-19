fed54ead9706367e13df9c496bb6bacf858c4468
willpaxton16 <90337820+willpaxton16@users.noreply.github.com>
2022-01-19 13:49:11 -0700

    Add project files.

---
+++ Mission3/Models/CalculatorModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission3.Models
+{
+    //Model to grab user inputs for Grade Calculator and perform some validation
+    public class CalculatorModel
+    {
+        [Required]
+        [Range(0,100)]
+        public decimal Assignments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal GroupProjects { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal Quizzes { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal Exams { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal INTEX { get; set; }
+
+    }
+}
---
+++ Mission3/wwwroot/js/JavaScript.js
@@ -0,0 +1,42 @@
+﻿
+$("#buttoncalc").click(function () {
+    /*Calculate Number Grade*/
+    var a = $("#atxt").val();
+    var g = $("#gptxt").val();
+    var q = $("#qtxt").val();
+    var e = $("#etxt").val();
+    var i = $("#itxt").val();
+    var grade = (a * .55) + (g * .05) + (q * .1) + (e * .2) + (i * .1);
+
+    /*Calculate Letter Grade*/
+    if (grade >= 94)
+        var letGrade = "A"
+    else if (grade >= 90)
+        var letGrade = "A-"
+    else if (grade >= 87)
+        var letGrade = "B+"
+    else if (grade >= 84)
+        var letGrade = "B"
+    else if (grade >= 80)
+        var letGrade = "B-"
+    else if (grade >= 77)
+        var letGrade = "C+"
+    else if (grade >= 74)
+        var letGrade = "C"
+    else if (grade >= 70)
+        var letGrade = "C-"
+    else if (grade >= 67)
+        var letGrade = "D+"
+    else if (grade >= 64)
+        var letGrade = "D"
+    else if (grade >= 60)
+        var letGrade = "D-"
+    else
+        var letGrade = "E"
+
+    /*Display both Number and Letter Grade*/
+    if (a > 0 & a <= 100 & g > 0 & g <= 100 & q > 0 & q <= 100 & e > 0 & q <= 100 & i > 0 & i <= 100)
+        alert("Grade: " + grade + "%\nLetter Grade: " + letGrade);
+});
+
+
